21c5e583efc9e9d44ba5af7455ff7bf8149bc712
Brian Voss <brian@brianvoss.com>
2012-06-30 22:19:55 -0700

    Added auto-population of email, phone

--- src/org/alljoyn/bus/sample/chat/StartScreen.java
+++ src/org/alljoyn/bus/sample/chat/StartScreen.java
@@ -1,10 +1,16 @@
 package org.alljoyn.bus.sample.chat;
 
+import java.util.regex.Pattern;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
-
+import android.widget.TextView;
+import android.telephony.TelephonyManager;
 
 public class StartScreen extends Activity {
 
@@ -14,6 +20,25 @@ public class StartScreen extends Activity {
         Log.i(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.startscreen);
+        
+        // Set the devices default phone number
+        String number = ((TelephonyManager) getSystemService(TELEPHONY_SERVICE))
+                .getLine1Number();
+        TextView Phoneno=(TextView)findViewById(R.id.st_phone);
+        Phoneno.setText(number);
+        
+        // Set the device's default email address
+        String possibleEmail = null;
+        Pattern emailPattern = Patterns.EMAIL_ADDRESS; // API level 8+
+        Account[] accounts = AccountManager.get(getBaseContext()).getAccounts();
+        for (Account account : accounts) {
+            if (emailPattern.matcher(account.name).matches()) {
+                possibleEmail = account.name;
+            }
+        }
+        TextView Email=(TextView)findViewById(R.id.st_email);
+        Email.setText(possibleEmail);
+        
 	}
 	
 	public void importBtn(View v) {
