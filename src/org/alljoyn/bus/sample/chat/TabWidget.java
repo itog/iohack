616a88ebb21bb3a7bd0ee67061f9fc759184aeac
korodroid <kanbara@gmail.com>
2012-07-01 11:40:25 +0900

    added ui change

--- src/org/alljoyn/bus/sample/chat/TabWidget.java
+++ src/org/alljoyn/bus/sample/chat/TabWidget.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011, Qualcomm Innovation Center, Inc.
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,6 +20,7 @@ import android.os.Bundle;
 
 import android.app.TabActivity;
 
+import android.widget.ListView;
 import android.widget.TabHost;
 
 import android.content.Intent;
@@ -39,9 +40,19 @@ public class TabWidget extends TabActivity {
         TabHost.TabSpec spec;
         Intent intent;
 
+
+        /*
         intent = new Intent().setClass(this, UseActivity.class);
         spec = tabHost.newTabSpec("use").setIndicator("", res.getDrawable(R.drawable.ic_tab_use)).setContent(intent);
         tabHost.addTab(spec);
+        */
+
+
+
+        intent = new Intent().setClass(this, ViewChannelActivity.class);
+        spec = tabHost.newTabSpec("viewchannel").setIndicator("", res.getDrawable(R.drawable.ic_tab_use)).setContent(intent);
+        tabHost.addTab(spec);
+
 
         intent = new Intent().setClass(this, HostActivity.class);
         spec = tabHost.newTabSpec("host").setIndicator("", res.getDrawable(R.drawable.ic_tab_host)).setContent(intent);
